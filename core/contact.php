a5d41fbdb4323d41a12f47b2c5db1f4f82aadc68
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2015-07-02 21:54:43 +0100

    Update contact.php

--- core/contact.php
+++ core/contact.php
@@ -1,3 +1,7 @@
 <?php
 
 include 'function.php';
+
+  $emailTo ="anyemail@email.com"; // This is the email you want the response to go to.
+  $emailFrom ="anyemail@email.com"; // This is to show where the email has come from and to stop errors with spam if using a black list.
+  $emailSubject = "Contact Me!"; // This is the the title for the contact
