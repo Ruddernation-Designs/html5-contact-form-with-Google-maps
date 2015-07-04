fa87b4614dc666903c68cbc1961962fb7ee504bd
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2015-07-04 13:44:31 +0100

    Added header functions

--- core/contact.php
+++ core/contact.php
@@ -22,14 +22,22 @@ if (!empty($_POST)){
   $_POST = multiDimensionalArrayMap('removebadtags', $_POST);
   $_POST = multiDimensionalArrayMap('eraseData', $_POST);
   $emailTo ="anyemail@email.com"; // This is the email you want the response to go to.
-  $emailFrom ="anyemail@email.com"; // This is to show where the email has come from and to stop errors with spam if using a black list.
-  $emailSubject = "Contact Us!"; // This is the the title for the contact
+  $emailfrom ="anyemail@email.com"; // This is to show where the email has come from and to stop errors with spam if using a black list.
+  $emailsubject = "Contact Us!"; // This is the the title for the contact
   $name = $_POST["name"];
   $email = $_POST["email"];
-  $comment = $_POST["comment"];
+  $message = $_POST["message"];
     if($name == "")
    $data['success'] = false;
  if (!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email)) 
    $data['success'] = false;
- if($comment == "")
+ if($message == "")
    $data['success'] = false;
+  $headers = "MIME-Version: 1.0" . "\r\n"; 
+  $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
+  $headers .= "From: <$emailfrom>" . "\r\n";
+  mail($emailTo, $emailsubject, $message, $headers);
+  $data['success'] = true;
+  echo json_encode($data);
+}
+}
