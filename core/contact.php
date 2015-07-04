4e84ac15698849ab28d301a31cb960fdc637213a
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2015-07-04 07:08:46 +0100

    Update contact.php

--- core/contact.php
+++ core/contact.php
@@ -1,10 +1,20 @@
 <?php
-
-include 'function.php';
-
+function multiDimensionalArrayMap($func,$arr) {
+  $newArr = array();
+  if (!empty($arr)) {
+    foreach($arr AS $key => $value) {
+      $newArr[$key] = (is_array($value) ? multiDimensionalArrayMap($func,$value) : $func($value));
+    }
+  }
+  return $newArr;
+}
 if (!empty($_POST)){
-
-  $data['Awesome, Message sent!'] = true; // This is the response to the sender if successful.
+  $data['success'] = true;
+  $_POST = multiDimensionalArrayMap('removebadtags', $_POST);
+  $_POST = multiDimensionalArrayMap('eraseData', $_POST);
   $emailTo ="anyemail@email.com"; // This is the email you want the response to go to.
   $emailFrom ="anyemail@email.com"; // This is to show where the email has come from and to stop errors with spam if using a black list.
-  $emailSubject = "Contact Me!"; // This is the the title for the contact
+  $emailSubject = "Contact Us!"; // This is the the title for the contact
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $comment = $_POST["comment"];
