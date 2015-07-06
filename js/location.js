f521fabbcc39b35a29757bf83c84d59eb5600b26
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2015-07-06 15:26:25 +0100

    Added mail functions.

--- js/location.js
+++ js/location.js
@@ -1,22 +1,91 @@
+// Mail functions
+$("#send-the-mail").click(function () {
+
+        var name = $('input#name').val();
+        var error = false;
+        if (name == "" || name == " ") {
+            $('#err-name').show(500);
+            $('#err-name').delay(4000);
+            $('#err-name').animate({
+                height: 'toggle'
+            }, 500, function () {
+            });
+            error = true; 
+        }
+        var emailCompare = /^([a-z0-9_.-]+)@([da-z.-]+).([a-z.]{2,6})$/;
+        var email = $('input#email').val().toLowerCase(); 
+        if (email == "" || email == " " || !emailCompare.test(email)) {
+            $('#err-email').show(500);
+            $('#err-email').delay(4000);
+            $('#err-email').animate({
+                height: 'toggle'
+            }, 500, function () {
+            });
+            error = true; 
+        }
+
+        var comment = $('textarea#comment').val();
+        if (comment == "" || comment == " ") {
+            $('#err-comment').show(500);
+            $('#err-comment').delay(4000);
+            $('#err-comment').animate({
+                height: 'toggle'
+            }, 500, function () {
+            });
+            error = true;
+        }
+
+        if (error == false) {
+            var dataString = $('#contact').serialize();
+            $.ajax({
+                type: "POST",
+                url: $('#contact').attr('action'),
+                data: dataString,
+                timeout: 6000,
+                error: function (request, error) {
+
+                },
+                success: function (response) {
+                    response = $.parseJSON(response);
+                    if (response.success) {
+                        $('#successSend').show();
+                        $("#name").val('');
+                        $("#email").val('');
+                        $("#comment").val('');
+                    } else {
+                        $('#errorSend').show();
+                    }
+                }
+            });
+            return false;
+        }
+
+        return false; 
+    });
+    jQuery('.contact').bind('inview', function (event, visible) {
+        if (visible == true) {
+            jQuery('.contact').addClass("animated bounceIn");
+        } else {
+            jQuery('.contact').removeClass("animated bounceIn");
+        }
+    });
 //You will need this to add your loaction via longitude and latitude on google maps also added the center position on longitude for if you keep the form over the map itself,
-//This also contains the zoom, I've done 17 as that shows the smaller street names.
+//This also contains the zoom.
 function initializeMap() {
 
     var lat = '51.5286416'; //Set your latitude, I've added London by default, not that I live there, ha.
     var lon = '-0.1015987'; //Set your longitude.
 
-    var centerLon = lon - 0.0025;
+    var centerLon = lon - 0.0035;
 
     var myOptions = {
         scrollwheel: false, // if true it will scroll when you scroll the mouse wheel.
         draggable: false, // if true this will move when you hold the mouse button.
-        disableDefaultUI: true, Default depending on googleapis
+        disableDefaultUI: true, // Default depending on googleapis
         center: new google.maps.LatLng(lat, centerLon), // this is the addition of the center line, change CenterLon to move the pointer.
-        zoom: 17, // Change this value to zoom in or out.
+        zoom: 16, // Change this value to zoom in or out.
         mapTypeId: google.maps.MapTypeId.ROADMAP // This is the road map version, can be changed using the googleapis.
     };
-
-    //Bind map to elemet with id map-canvas
     var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
     var marker = new google.maps.Marker({
         map: map,
