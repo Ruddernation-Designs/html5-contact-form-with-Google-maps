77ebcb17266996f4ae7eede942e24a405519bc0e
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2015-07-06 19:12:52 +0100

    Update contact.php

--- core/contact.php
+++ core/contact.php
@@ -7,9 +7,12 @@ function removebadtags($data) {
   $data = preg_replace("/onmouseover/i", "&#111;nmouseover",$data);
   $data = preg_replace("/onload/i", "&#111;nload",$data);
   $data = preg_replace("/onclick/i", "&#111;nclick",$data);
+  $data = preg_replace("/document\./i", "&#100;ocument.",$data);
+  $data = preg_replace("/javascript/i", "j&#097;v&#097;script",$data);
+  $data = preg_replace("/alert/i", "&#097;lert",$data);
   return strip_tags(trim($data));
 }
-function eraseData($data) {
+function erasedata($data) {
   $data = str_replace(' & ', ' &amp; ', $data);
   return (get_magic_quotes_gpc() ? stripslashes($data) : $data);
 }
@@ -24,28 +27,28 @@ function multiDimensionalArrayMap($func,$arr) {
 }
 if (!empty($_POST)){
   $data['success'] = true;
-  $_POST = multiDimensionalArrayMap('removebadtags', $_POST);
-  $_POST = multiDimensionalArrayMap('eraseData', $_POST);
-  $emailTo ="anyemail@email.com"; // This is the email you want the response to go to.
-  $emailfrom ="anyemail@email.com"; // This is to show where the email has come from and to stop errors with spam if using a black list.
-  $emailsubject = "Contact Us!"; // This is the the title for the contact
+  $_POST  = multiDimensionalArrayMap('removebadtags', $_POST);
+  $_POST  = multiDimensionalArrayMap('erasedata', $_POST);
+  $emailto ="support@ruddernation.com";
+  $emailfrom ="support@ruddernation.com"; 
+  $emailsubject = "Mail from Abbies site";
   $name = $_POST["name"];
   $email = $_POST["email"];
-  $message = $_POST["message"];
+  $comment = $_POST["comment"];
   if($name == "")
    $data['success'] = false;
  if (!preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email)) 
    $data['success'] = false;
- if($message == "")
+ if($comment == "")
    $data['success'] = false;
  if($data['success'] == true){
-  $message = "Name: $name<br>
-  Email: $email<br>
-  Message: $message";
+  $message = "Name: $name<br />
+  Email: $email<br />
+  Comment: $comment";
   $headers = "MIME-Version: 1.0" . "\r\n"; 
   $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
   $headers .= "From: <$emailfrom>" . "\r\n";
-  mail($emailTo, $emailsubject, $message, $headers);
+  mail($emailto, $emailsubject, $message, $headers);
   $data['success'] = true;
   echo json_encode($data);
 }
