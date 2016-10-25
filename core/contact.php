0db9a77252978bb37bad208b561d5f91271335e1
Ruddernation Designs <Ruddernation-Designs@users.noreply.github.com>
2016-10-25 10:40:32 +0200

    update to add scripts to separate file
    I've done this because there seemed to of been an error in message sending, Hopefully the edit will work as I've not checked this.

--- core/contact.php
+++ core/contact.php
@@ -1,30 +1,7 @@
 <?php
-function removebadtags($data) {
-  $data = preg_replace("/<html/i", "&lt;html",$data);
-  $data = preg_replace("/<body/i", "&lt;body",$data);
-  $data = preg_replace("/<script/i", "&lt;&#115;cript",$data);
-  $data = preg_replace("/onsubmit/i", "&#111;nsubmit",$data);
-  $data = preg_replace("/onmouseover/i", "&#111;nmouseover",$data);
-  $data = preg_replace("/onload/i", "&#111;nload",$data);
-  $data = preg_replace("/onclick/i", "&#111;nclick",$data);
-  $data = preg_replace("/document\./i", "&#100;ocument.",$data);
-  $data = preg_replace("/javascript/i", "j&#097;v&#097;script",$data);
-  $data = preg_replace("/alert/i", "&#097;lert",$data);
-  return strip_tags(trim($data));
-}
-function erasedata($data) {
-  $data = str_replace(' & ', ' &amp; ', $data);
-  return (get_magic_quotes_gpc() ? stripslashes($data) : $data);
-}
-function multiDimensionalArrayMap($func,$arr) {
-  $newArr = array();
-  if (!empty($arr)) {
-    foreach($arr AS $key => $value) {
-      $newArr[$key] = (is_array($value) ? multiDimensionalArrayMap($func,$value) : $func($value));
-    }
-  }
-  return $newArr;
-}
+
+include 'scripts.php';
+
 if (!empty($_POST)){
   $data['success'] = true;
   $_POST  = multiDimensionalArrayMap('removebadtags', $_POST);
@@ -36,7 +13,7 @@ if (!empty($_POST)){
   $email = filter_input(INPUT_POST,"email");
   $comment = filter_input(INPUT_POST,"comment");
   $captcha = filter_input(INPUT_POST,'g-recaptcha-response');
-  $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=Add-your-secret-key-here&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+  $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=Add-your-secret-key-here&response=".$captcha."&remoteip=".filter_input(INPUT_SERVER,'REMOTE_ADDR')), true);
   if($captcha == "")
   $data['success'] = false;
   if($name == "")
